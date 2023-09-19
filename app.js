a1649e9343276657151bb6428912883f60e89bf9
Sabuj <asbsabuj1214@gmail.comgit config --global user.name Sabujgit commit -m 'first push'git config -global color.ui autogit config --global user.email asbsabuj1214@gmail.com>
2023-09-19 19:29:46 +0600

    added new functions

--- app.js
+++ app.js
@@ -1,44 +1,32 @@
 const express = require("express");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
-const Blog = require("./models/blog");
+const blogRoutes = require("./routes/blogRoutes");
 
 const app = express();
 
 const dbURI =
   "mongodb+srv://tutorialguy:testing4578@fullwork.luiol6m.mongodb.net/Node-practice?retryWrites=true&w=majority";
 mongoose
-  .connect(dbURI)
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then((result) => app.listen(3000))
   .catch((err) => console.log(err));
 
 app.set("view engine", "ejs");
 
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   res.redirect("/blogs");
 });
 
-app.get("/blogs", (req, res) => {
-  Blog.find()n
-    .sort({ createdAt: -1 })
-    .then((result) => {
-      res.render("index", { title: "All Blogs", blogs: result });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
 app.get("/about", (req, res) => {
   res.render("about", { title: "about page" });
 });
 
-app.get("/blogs/create", (req, res) => {
-  res.render("create", { title: "create new blog" });
-});
+app.use("/blogs", blogRoutes);
 
 app.use((req, res) => {
   res.status(404).render("404", { title: 404 });
